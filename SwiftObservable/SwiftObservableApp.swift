4399259360e0c8513c497d0dd2ef85a287da1840
Javid <Javid.d@780.team>
2024-01-15 17:24:29 +0330

    Initial Commit

---
+++ SwiftObservable/SwiftObservableApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftObservableApp.swift
+//  SwiftObservable
+//
+//  Created by Javid on 1/15/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftObservableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
